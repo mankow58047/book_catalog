83c0e24c3b864094ab2c7d7a0697bb22d4182018
Cindy Manikowski <mankow58047@gmail.com>
2022-07-12 11:41:36 -0500

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+#app/__int__.py
+
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.login_view = 'authentication.do_the_login'
+login_manager.session_protection = 'strong'
+bcrypt = Bcrypt()
+
+def create_app(config_type): #dev, text or prod
+
+    app = Flask(__name__)
+    configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
+    app.config.from_pyfile(configuration)
+    db.init_app(app)    # bind database to flask app
+    bootstrap.init_app(app) # initialize bootstrap
+    login_manager.init_app(app) # initialize Login manager
+    bcrypt.init_app(app) # initialize bcrypt manager
+
+    from app.catalog import main
+    app.register_blueprint(main)
+
+    from app.auth import authentication
+    app.register_blueprint(authentication)
+
+    return app
+
